c8a06df8a9dc612a3eb2b1c3d122de3b957f7449
Uday Teja <62304758+uday510@users.noreply.github.com>
2026-04-13 21:33:16 +0530

    add default route

--- api/default.go
+++ api/default.go
@@ -5,6 +5,6 @@ import "net/http"
 func DefaultHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "text/plain")
-	w.Write([]byte("welcome to earth, human. you seem lost.\n")
+	w.Write([]byte("welcome to earth, human. you seem lost.\n"))
 
 }
